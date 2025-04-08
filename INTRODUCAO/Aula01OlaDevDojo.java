7be10d92bf36dac5e4303ef848eb3016c774dbda
Ricardo Santos <kadubass@hotmail.com>
2025-04-08 10:03:04 -0300

    Update Aula01OlaDevDojo.java
    breve alteração no código para teste git pull

--- INTRODUCAO/Aula01OlaDevDojo.java
+++ INTRODUCAO/Aula01OlaDevDojo.java
@@ -2,6 +2,6 @@ package INTRODUCAO;
 
 public class Aula01OlaDevDojo {
     public static void main (String[]args){
-        System.out.println("KA-ME-HA-ME-HA");
+        System.out.println("KA-ME-HA-ME-HA");Funcionou
     }
 }
