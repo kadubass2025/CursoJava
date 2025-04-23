c63baed313a576eaec453bcbe5ca2e6fe474d707
kadubass2025 <kadubass@hotmail.com>
2025-04-23 17:52:40 -0300

    Inclusão aula estrutura de repetição

--- INTRODUCAO/Aula06EstruturasDeRepetição01.java
+++ INTRODUCAO/Aula06EstruturasDeRepetição01.java
@@ -2,6 +2,25 @@ package INTRODUCAO;
 
 public class Aula06EstruturasDeRepetição01 {
     public static void main(String[] args) {
-        
+        // while, do while , for
+
+        int cont = 0;
+        while (cont <= 10){
+            System.out.print( cont + " ");
+            cont++;
+        }
+        System.out.println();
+        cont = 0;
+        do {
+            System.out.println("Aqui é dentro do DO WHILE");
+            cont++;
+        }while (cont <= 10);
+
+        for (int i=0; i<=10; i++){
+            System.out.print("For" + i + " ");
+        }
+
+
+        }
     }
-}
+
