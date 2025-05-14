e5c56c2c4b410a4a854d77aa6d7315e3a3e014dd
kadubass2025 <kadubass@hotmail.com>
2025-05-14 19:43:15 -0300

    Inclusão SET/GET classe funcionário

--- INTRODUCAO/JavaCore/BintroducaoMetodos/Test/FuncionarioTest01.java
+++ INTRODUCAO/JavaCore/BintroducaoMetodos/Test/FuncionarioTest01.java
@@ -5,11 +5,11 @@ import INTRODUCAO.JavaCore.BintroducaoMetodos.dominio.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Stark Federal";
-        funcionario.idade = 40;
-        funcionario.salarios = new double[]{1200, 5000, 12000};
+        funcionario.setNome("Stark Federal");
+        funcionario.setIdade(40);
+        funcionario.setSalarios(new double[]{1200, 5000, 12000});
 
         funcionario.imprime();
-        funcionario.imprimeMediaSalarios();
+
     }
 }
--- INTRODUCAO/JavaCore/BintroducaoMetodos/dominio/Funcionario.java
+++ INTRODUCAO/JavaCore/BintroducaoMetodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package INTRODUCAO.JavaCore.BintroducaoMetodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[]salarios;
+    private String nome;
+    private int idade;
+    private double[]salarios;
+    private double media;
 
     public void imprime(){
         System.out.println(this.nome);
@@ -13,11 +14,42 @@ public class Funcionario {
         }
     }
     public void imprimeMediaSalarios(){
-        double media = 0;
-        for(double salario : salarios){
+            for(double salario : salarios){
             media += salario;
         }
         media /= salarios.length;
         System.out.println("\nMédia Salarial: "+media);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
